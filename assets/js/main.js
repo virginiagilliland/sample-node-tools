b4d3b2f826e16ea27ba73d813d1af2096af523c4
Owen Mundy <omundy@gmail.com>
2021-03-18 14:08:47 -0400

    Clean up D3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,27 @@
-
-
 fetch(`node-projects/convert-data/data.json`)
 	.then(response => response.json())
 	.then(data => {
 		console.log(data[0]);
-
-		let str = "";
-
-		for (let i = 0; i < data.length; i++) {
-			str += `<p>${data[i].name}.${data[i].purpose}</p>`;
-		}
-
-		d3.selectAll(".data")
-			.data(data)
-			.enter()
-			.append("p")
-			.text(function(d) {
-				return d.name;
-
-			});
-
+		displayData(data);
 	});
+
+
+function displayData(data) {
+
+	// let str = "";
+	// for (let i = 0; i < data.length; i++) {
+	// 	str += `<p>${data[i].name}.${data[i].purpose}</p>`;
+	// }
+
+	// reference to element
+	var dataEle = d3.select(".data");
+	// adds div for each data item
+	dataEle.selectAll("div")
+		.data(data)
+		.enter()
+		.append("div")
+		.text(function(d) {
+			return `${d.purpose} - ${d.name}`;
+		});
+
+}
